9a21ddea7e4d24ca94aa54acac39b09641f18206
Om-Bhandarkar <ombhandarkar1206@gmail.com>
2023-10-15 23:45:47 +0530

    update

--- BASIC/Class_Object/5.dart
+++ BASIC/Class_Object/5.dart
@@ -1,6 +1,6 @@
 class Point{
- int x;    // Non-nullable instance field 'x' must be initialized.
- int y;    // Non-nullable instance field 'y' must be initialized.
+ int x = 10;    // Non-nullable instance field 'x' must be initialized.
+ int y = 20;    // Non-nullable instance field 'y' must be initialized.
 }
 void main(){
   Point obj = new Point();    // Error
